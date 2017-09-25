8ba220fbb829452fac66b1c6393a738dc5bd27de
Maxim Krizanovskiy <just_my_mail@coolsite.net>
2017-09-25 13:44:36 +0300

    v.0.1

--- Ex1/Ex1/Source.cpp
+++ Ex1/Ex1/Source.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 void main() {
 	setlocale(LC_ALL, "Rus");
-	unsigned int n, size, sum1 = 0;
+	unsigned int n, size, size2,size1 = 2, sum1 = 0;
+	bool dex = false;
 	srand(time(NULL));
 	n = 2 + rand() % 11;
+	size2 = n - 1;
 	cout << "Человек в каяке: "<< n << endl;
 	size = 2 * n;
 	unsigned int *quin = new unsigned int[size];
@@ -18,16 +20,29 @@ void main() {
 		quin[i] = 1 + rand() % 1001;
 		cout << "Вес " << i + 1 << ": " << quin[i] << endl;
 	}
-		int i = size;
-		while (i == 0)
-		{
-			for (int j = 0; j < 2; j++)
-			{
-				sum2[i] += quin[i];
-				i--;
+	while (!dex) {
+		dex = true;
+		int temp;
+		for (int i = 0; i < size - 1; i++) {
+			if (quin[i] > quin[i + 1]) {
+				temp = quin[i];
+				quin[i] = quin[i + 1];
+				quin[i + 1] = temp;
+				dex = false;
 			}
+		}
+	}
+	/*int i = size-3;
+	while (i >= 0)
+	{
+		int c = 0;
+		for (int j = 0; j < 2; j++)
+		{
+			sum2[c] += quin[i];
 			i--;
 		}
+		c++;
+	}*/
 	for (int j = 0; j < n; j++)
 	{
 		cout << "Сумма n-человек: " << sum2[j] << endl;
